641fd3dcc645e41de9fc995fd37c2575722598d2
farid67 <farid.ouhmich@hotmail.fr>
2021-01-21 00:36:09 +0100

    resize image pyhon code

---
+++ resize_images.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python                                                  
+from PIL import Image                                              
+import os, sys                       
+
+path =  "/home/farid/Documents/These/Redaction/Contributions/images/ImagingTraits/"   
+dirs = os.listdir(path)                                       
+
+def resize():
+    for item in dirs:
+        if item.split(".")[-1] == "png":
+            print (item)
+            im = Image.open(os.path.join(path, item))
+            x,y = im.size
+            imResize = im.resize((int(x/2),int(y/2)), Image.ANTIALIAS)
+            imResize.save(os.path.join(path, "Resize" + item), 'png', quality=80)
+
+resize()
